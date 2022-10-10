2812f86ce31b5e703da5d713d4b4e8349b0e1e47
Rose Maina <rosewairimu27@gmail.com>
2022-10-10 12:14:15 +0300

    Created Navbar Components

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar1 from './components/Navbar1'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar1 />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
