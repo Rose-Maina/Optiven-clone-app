f1917d59f3aa550cee0b402ccc80eec818d4bfca
Rose Maina <rosewairimu27@gmail.com>
2022-10-18 10:06:34 +0300

    Edited the Navbars

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,50 +1,25 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='Tel2'>
       <Container>
-        <Navbar.Brand href="#home">Premium Properties</Navbar.Brand>
+        <Navbar.Brand href="/">Company Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            {/* <Nav.Link href="#pricing">About-Us</Nav.Link> */}
-            <NavDropdown title="About-Us" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Who We Are</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Our Team</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Why Us</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Awards & Milestones</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Our Partners</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Corporate Social Responsibility</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Properties" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Properties In Nairobi</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Properties In Nyeri</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Properties In Kisumu</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Properties in Nakuru</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Past Projects
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Information-Center" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Customer Information</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">News & Blogs</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Careers</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Photo Gallery</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">FAQs</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+            <Nav.Link as={Link} to={"/about"}>About Us</Nav.Link>
+            <Nav.Link as={Link} to={"/property"}>Properties</Nav.Link>
+            <Nav.Link as={Link} to={"/informationCentre"}>Information centre</Nav.Link>
+            <Nav.Link as={Link} to={"/testimonials"}>Testimonials</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link eventKey={2} href="#contacts">
-              Contacts
-            </Nav.Link>
+            <Nav.Link as={Link} to={"/contact"}>Contacts</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- my-app/src/components/Navbar1.js
+++ my-app/src/components/Navbar1.js
@@ -7,11 +7,6 @@ function TextLinkExample() {
         <Container>
          <Navbar.Brand href="#home">To Own Property Call or Sms:+25412345678</Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Rose Maina</a>
-        </Navbar.Text>
-       </Navbar.Collapse>
      </Container>
     </Navbar>
   );
