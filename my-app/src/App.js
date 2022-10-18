553ad5435dbf24ac30d8038b1e491aea6a2015d7
Rose Maina <rosewairimu27@gmail.com>
2022-10-18 10:07:10 +0300

    Updated the Apps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
 import './App.css';
-import Navbar1 from './components/Navbar1';
+ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Navbar1 from './components/Navbar1';
+import About from './components/About';
+import Home from './components/Home';
+import Testimonials from './components/Testimonials.js';
+import Contact from './components/Contact';
+import Properties from './components/Properties';
+import InformationCenter from './components/InformationCentre';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
+    <>
     <div className="App">
-      <Navbar1 />
-      <Navbar />
+      <Navbar1/>
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="about" element={<About/>}></Route>
+          <Route path="property" element={<Properties/>}></Route>
+          <Route path="/InformationCentre" element={<InformationCenter/>}></Route>
+          <Route path="/testimonials" element={<Testimonials/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
     </div>
+    </>
   );
 }
 
